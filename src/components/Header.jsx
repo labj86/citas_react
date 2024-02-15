48fa15d7584bbe064870ff68fb0e33e985ce669e
Luis Alejandro Barranco Juárez <labj86@gmail.com>
2024-02-15 00:53:25 -0600

    Poco trabajo pero honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ function Header() {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-3/4 mx-auto">
-        Seguimiento Pacientes {''} <br />
-        <span className="text-indigo-600">Veterinaria</span>
+        Seguimiento de Pacientes {''} <br />
+        <span className="text-indigo-700">Veterinaria</span>
       </h1>
     </>
   )
