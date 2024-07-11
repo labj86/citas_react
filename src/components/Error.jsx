7103d820991a0818afa32a13d0cff2b35b53bc95
Luis Alejandro Barranco Juárez <labj86@gmail.com>
2024-07-10 22:46:51 -0600

    Se agrego zustand y reactHookForm. Cambio a TS

--- src/components/Error.jsx
+++
@@ -1,8 +0,0 @@
-export const Error = ({children}) => {
-  return (
-    <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md'>
-      {/* <h1>Prueba</h1> */}
-      {children}
-    </div>
-  )
-}
